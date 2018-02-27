8031ccd49f66f0a45c6b6a975c7ce7d2587bb9ed
tony-h-lee <tonyhjlee@gmail.com>
2018-02-27 12:48:57 -0500

    removed semantic and added bootstrap

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,14 +11,20 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Button } from 'react-bootstrap';
 import messages from './messages';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+        <p> Hello </p>
+        <Button bsStyle="primary">Primary</Button>
+        <Button bsStyle="info">Info</Button>
+      </div>
     );
   }
 }
