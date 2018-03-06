e4116f482c83cdf2cc4749c1e65322c6060eb05e
tony-h-lee <tonyhjlee@gmail.com>
2018-03-06 14:45:27 -0500

    forgot password client api

--- app/containers/ForgotPasswordPage/api.js
+++ app/containers/ForgotPasswordPage/api.js
@@ -1,11 +1,11 @@
 import Request from 'utils/request';
 
 export function forgotPassword(values) {
-  const url = 'http://localhost:9000/api/reset-password';
+  const url = 'http://localhost:9000/api/password-reset';
   return new Request()
     .setUrl(url)
     .setPost()
     .addHeader({ 'Content-Type': 'application/json' })
-    .setBody(values)
+    .setBody({ ...values, link: '/reset-password' })
     .go();
 }
