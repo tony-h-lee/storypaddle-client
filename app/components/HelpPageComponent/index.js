3f2d0f64be5847b684a62620e2a617d6d676931c
tony-h-lee <tonyhjlee@gmail.com>
2018-05-16 17:37:26 -0400

    page templates

--- app/components/HelpPageComponent/index.js
+++ app/components/HelpPageComponent/index.js
@@ -5,13 +5,23 @@
 */
 
 import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
 // import styled from 'styled-components';
 
 
 function HelpPageComponent() {
   return (
-    <div>
-    </div>
+    <Container text style={{ paddingTop: '3rem', paddingbottom: '3rem' }}>
+      <Header
+        as="h1"
+        content="Contact Us!"
+      />
+      <p>
+        Please send us an email regarding an issue or for any questions you may have. We will do our best to respond
+        in a timely manner.
+      </p>
+      <p> Send your email to - <b> help@storypaddle.com </b> </p>
+    </Container>
   );
 }
 
